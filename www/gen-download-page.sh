ec447d28f635b56c5849ade82455b5d9c0d6d236
Michał Łyszczek <michal.lyszczek@bofc.pl>
2019-11-27 12:20:41 +0100

    www/gen-download-page.sh: add support for *-r1 version
    Signed-off-by: Michał Łyszczek <michal.lyszczek@bofc.pl>

--- www/gen-download-page.sh
+++ www/gen-download-page.sh
@@ -48,9 +48,8 @@ echo "<h1>tarballs (source code)</h1>"
 echo "<pre>"
 
 files="$(get_files_from_remote "${remote}/" | \
-    grep "${project}-[0-9]*\.[0-9]*\.[0-9][\.-]\(r[0-9]\)\?")"
-versions="$(echo "${files}" | tr ' ' '\n' | rev | \
-    cut -f1 -d- | rev | cut -f1-3 -d. | sort -Vur)"
+    grep -P "${project}-\d+\.\d+\.\d+(-r\d+)?")"
+versions="$(echo "${files}" | tr ' ' '\n' | grep -Po "\d+\.\d+\.\d+(-r\d+)?")"
 
 for v in ${versions}
 do
